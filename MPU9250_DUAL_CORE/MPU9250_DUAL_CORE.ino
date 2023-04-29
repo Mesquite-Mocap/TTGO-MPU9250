c0eb6d589467f82956a01c3a870e3e63514a7334
Tejaswi Gowda <tejaswi@asu.edu>
2023-04-29 12:03:30 -0700

    good version

--- MPU9250_DUAL_CORE/MPU9250_DUAL_CORE.ino
+++ MPU9250_DUAL_CORE/MPU9250_DUAL_CORE.ino
@@ -1,6 +1,5 @@
 #include <MPU9250.h>
 #include "esp_adc_cal.h"
-#include "image.h"
 #include "charge.h"
 #include <TFT_eSPI.h>
 #include <pcf8563.h>
@@ -33,6 +32,8 @@ unsigned long lastTime = 0;
 unsigned long timerDelay = 1000 * 60 * 2;
 
 String batt_level = "";
+bool displayOn = true;
+bool lastTouch = false;
 
 bool otaMode = false;
 
@@ -126,11 +127,13 @@ void configModeCallback(WiFiManager *myWiFiManager) {
   Serial.println(myWiFiManager->getConfigPortalSSID());
 
   tft.fillScreen(TFT_BLACK);
-  tft.setTextColor(TFT_WHITE);
-  tft.drawString("Connect to hotspot: ", 0, tft.height() / 2 - 20);
-  tft.drawString("configure wrist", 35, tft.height() / 2 + 20);
   tft.setTextColor(TFT_GREEN);
-  tft.drawString("\"" + mac_address + "\"", 10, tft.height() / 2);
+  tft.setTextDatum(MC_DATUM);
+  tft.drawString("OTA Mode", tft.width() / 2, tft.height() / 2 - 20);
+  //tft.drawString("configure wrist", tft.width()/2, tft.height() / 2 + 20);
+  tft.setTextColor(TFT_WHITE);
+  tft.setTextDatum(MC_DATUM);
+  tft.drawString(mac_address, tft.width() / 2, tft.height() / 2);
 }
 
 void drawProgressBar(uint16_t x0, uint16_t y0, uint16_t w, uint16_t h, uint8_t percentage, uint16_t frameColor, uint16_t barColor) {
@@ -227,6 +230,7 @@ void setupADC() {
 }
 
 String Bone = "N/A";
+bool calibrated = false;
 
 class MyCallbacks : public BLECharacteristicCallbacks {
   void onWrite(BLECharacteristic *pChar) {
@@ -234,18 +238,65 @@ class MyCallbacks : public BLECharacteristicCallbacks {
     Serial.print("Received Value: ");
     Serial.println(value.c_str());
 
-    if (value == "calibrate") {
+
+    if (value == "start") {
       tft.fillScreen(TFT_BLACK);
       tft.setTextColor(TFT_WHITE, TFT_BLACK);
-      tft.drawString(mac_address, 20, 1);
-      tft.setTextColor(TFT_RED, TFT_BLACK);
-      tft.drawString("CONNECTED", 20, tft.height() / 2);
+      tft.setTextDatum(MC_DATUM);
+      tft.drawString(mac_address, tft.width() / 2, 10);
+      tft.setTextColor(TFT_GREEN, TFT_BLACK);
+      tft.drawString("CONNECTED", tft.width() / 2, tft.height() / 2);
+      tft.setTextColor(TFT_ORANGE, TFT_BLACK);
+      tft.drawString("Calibrate now", tft.width() / 2, tft.height() - 20);
+
+    } else if (value == "calibrate") {
       start = true;
-    } else if (value == "ota") {
+    }
+    else if(value == "dispoff"){
+      if(calibrated){
+              digitalWrite(TFT_BL, LOW);
+        tft.writecommand(ST7735_DISPOFF);
+        displayOn = false;
+      }
+
+    } 
+        else if(value == "poweroff"){
+                tft.fillScreen(TFT_BLACK);
+          tft.setTextColor(TFT_RED, TFT_BLACK);
+          tft.setTextDatum(MC_DATUM);
+          tft.drawString("POWER OFF", tft.width() / 2, tft.height() / 2);
+          // mpu.setSleepEnabled(true);
+          mpu.sleep(true);
+          Serial.println("Go to Sleep");
+          delay(3000);
+          tft.writecommand(ST7735_SLPIN);
+          tft.writecommand(ST7735_DISPOFF);
+          esp_sleep_enable_ext1_wakeup(GPIO_SEL_33, ESP_EXT1_WAKEUP_ANY_HIGH);
+          esp_deep_sleep_start();
+    } 
+
+    else if(value == "restart"){
+          tft.fillScreen(TFT_BLACK);
+          tft.setTextColor(TFT_RED, TFT_BLACK);
+          tft.setTextDatum(MC_DATUM);
+          tft.drawString("POWER OFF", tft.width() / 2, tft.height() / 2);
+                    delay(3000);
+            ESP.restart();
+    } 
+    
+    else if (value == "ota") {
+          tft.fillScreen(TFT_BLACK);
+      tft.setTextColor(TFT_GREEN, TFT_BLACK);
+      tft.setTextDatum(MC_DATUM);
+      tft.drawString("Rebooting", tft.width() / 2, 30);
+      tft.drawString("in OTA mode....", tft.width() / 2, 50);
+
       EEPROM.write(0, 1);
       EEPROM.commit();
       delay(2000);
       ESP.restart();
+    } else {
+      Bone = String(value.c_str());
     }
   }
 };
@@ -268,9 +319,17 @@ void setup() {
   tft.init();
   tft.setRotation(1);
   tft.setSwapBytes(true);
-  tft.pushImage(0, 0, 160, 80, ttgo);
-  tft.setFreeFont(FSB9);
+  //tft.pushImage(0, 0, 160, 80, ttgo);
 
+
+  tft.setFreeFont(FF17);
+
+  tft.fillScreen(TFT_BLACK);
+  tft.setTextColor(random(0xFFFF), TFT_BLACK);
+  tft.setTextDatum(MC_DATUM);
+  tft.drawString("Mesquite Mocap", tft.width() / 2, 20);
+  tft.drawString("https://mesquite.cc", tft.width() / 2, tft.height() - 40);
+  tft.setTextColor(TFT_BLUE, TFT_BLACK);
 
 
   pinMode(TP_PIN_PIN, INPUT);
@@ -339,15 +398,25 @@ void setup() {
 
 
 
+  batt_level = getVoltage();
 
   tft.fillScreen(TFT_BLACK);
   tft.setTextColor(TFT_WHITE, TFT_BLACK);
-  tft.drawString(mac_address, 10, 10);
-  tft.setTextColor(TFT_RED, TFT_BLACK);
-  tft.drawString("Ready to Connect....", 0, tft.height() / 2);
+  tft.setTextDatum(MC_DATUM);
+  tft.drawString(mac_address, tft.width() / 2, 10);
+  tft.setTextColor(TFT_GREEN, TFT_BLACK);
+  tft.setTextDatum(MC_DATUM);
+  tft.drawString("Connect at:", tft.width() / 2, 30);
+  tft.setTextColor(TFT_YELLOW, TFT_BLACK);
+  tft.setTextDatum(MC_DATUM);
+  tft.drawString("https://mesquite.cc", tft.width() / 2, 45);
+  tft.setTextColor(TFT_BLUE, TFT_BLACK);
+
+  tft.drawString(batt_level, tft.width() / 2, tft.height() - 10);
+
+  batt_level = ", " + getVoltage();
 
   while (!start) {
-    Serial.print(".");
     if (digitalRead(TP_PIN_PIN) == HIGH) {
       if (!pressed) {
         pressed = true;
@@ -357,13 +426,13 @@ void setup() {
       if (millis() - pressedTime > 2000) {
         if (digitalRead(TP_PIN_PIN) == HIGH) {
           tft.fillScreen(TFT_BLACK);
-          tft.setTextColor(TFT_GREEN, TFT_BLACK);
+          tft.setTextColor(TFT_RED, TFT_BLACK);
           tft.setTextDatum(MC_DATUM);
-          tft.drawString("Turning off....", tft.width() / 2, tft.height() / 2);
+          tft.drawString("POWER OFF", tft.width() / 2, tft.height() / 2);
           // mpu.setSleepEnabled(true);
           mpu.sleep(true);
           Serial.println("Go to Sleep");
-          delay(5000);
+          delay(3000);
           tft.writecommand(ST7735_SLPIN);
           tft.writecommand(ST7735_DISPOFF);
           esp_sleep_enable_ext1_wakeup(GPIO_SEL_33, ESP_EXT1_WAKEUP_ANY_HIGH);
@@ -386,11 +455,12 @@ void setup() {
   Serial.println("Accel Gyro calibration will start in 5sec.");
   tft.fillScreen(TFT_BLACK);
   tft.setTextColor(TFT_WHITE, TFT_BLACK);
-  tft.drawString(mac_address, 10, 0);
+  tft.setTextDatum(MC_DATUM);
+  tft.drawString(mac_address, tft.width() / 2, 10);
   tft.setTextColor(TFT_ORANGE, TFT_BLACK);
-  tft.drawString("CALIBRATE (1)", 10, tft.height() / 2.5);
+  tft.drawString("CALIBRATION (1)", tft.width() / 2, tft.height() / 2.5);
 
-  tft.drawString("Leave still..", 10, tft.height() / 1.5);
+  tft.drawString("Keep flat and still..", tft.width() / 2, tft.height() / 1.5);
   Serial.println("Please leave the device still on the flat plane.");
   mpu.verbose(true);
   delay(5000);
@@ -399,27 +469,30 @@ void setup() {
 
   tft.fillScreen(TFT_BLACK);
   tft.setTextColor(TFT_WHITE, TFT_BLACK);
-  tft.drawString(mac_address, 10, 0);
-  tft.setTextColor(TFT_PURPLE, TFT_BLACK);
-  tft.drawString("CALIBRATE (2)", 10, tft.height() / 2.5);
+  tft.setTextDatum(MC_DATUM);
+  tft.drawString(mac_address, tft.width() / 2, 10);
+  tft.setTextColor(TFT_GREENYELLOW, TFT_BLACK);
+  tft.drawString("CALIBRATION (2)", tft.width() / 2, tft.height() / 2.5);
 
-  tft.drawString("Wave in 8 pattern", 10, tft.height() / 1.5);
+  tft.drawString("Wave in 8 pattern", tft.width() / 2, tft.height() / 1.5);
   Serial.println("Please Wave device in a figure eight until done.");
   delay(2000);
   mpu.calibrateMag();
 
   mpu.verbose(false);
 
+  calibrated = true;
+
   batt_level = ", " + getVoltage();
 
 
   tft.fillScreen(TFT_BLACK);
   tft.setTextColor(TFT_WHITE, TFT_BLACK);
-  tft.drawString(mac_address, 10, 0);
+  tft.drawString(mac_address, tft.width() / 2, 10);
   tft.setTextColor(TFT_GREEN, TFT_BLACK);
-  tft.drawString("Transmitting Data", 10, tft.height() / 2.5);
+  tft.drawString("Transmitting Data", tft.width() / 2, tft.height() / 2.5);
   tft.setTextColor(TFT_BLUE, TFT_BLACK);
-  tft.drawString(Bone + batt_level, 10, tft.height() / 1.5);
+  tft.drawString(Bone + batt_level, tft.width() / 2, tft.height() / 1.5);
 
 
   pinMode(TP_PIN_PIN, INPUT);
@@ -458,8 +531,7 @@ void setup() {
     NULL, 1);
 }
 
-bool displayOn = true;
-bool lastTouch = false;
+
 
 
 void loop() {
@@ -479,13 +551,13 @@ void loop() {
     if (millis() - pressedTime > 4000) {
       if (digitalRead(TP_PIN_PIN) == HIGH) {
         tft.fillScreen(TFT_BLACK);
-        tft.setTextColor(TFT_GREEN, TFT_BLACK);
+        tft.setTextColor(TFT_RED, TFT_BLACK);
         tft.setTextDatum(MC_DATUM);
-        tft.drawString("Turning off...", tft.width() / 2, tft.height() / 2);
+        tft.drawString("POWER OFF", tft.width() / 2, tft.height() / 2);
         // mpu.setSleepEnabled(true);
         mpu.sleep(true);
         Serial.println("Go to Sleep");
-        delay(5000);
+        delay(3000);
         tft.writecommand(ST7735_SLPIN);
         tft.writecommand(ST7735_DISPOFF);
         esp_sleep_enable_ext1_wakeup(GPIO_SEL_33, ESP_EXT1_WAKEUP_ANY_HIGH);
